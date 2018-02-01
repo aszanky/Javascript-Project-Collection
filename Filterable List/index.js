46ae9c6af2691b3629c1b3bb39ce050dbd2340f1
asaduddin <asaduddin.z@samsung.com>
2018-02-01 12:03:55 +0700

    edit little in a

--- Filterable List/index.js
+++ Filterable List/index.js
@@ -8,11 +8,18 @@ filterInput.addEventListener('keyup', function(e) {
     let li = ul.querySelectorAll('li.collection-item');
 
     console.log(li)
+
+    console.log(li)
     for (let i = 0; i < li.length; i++) {
-        let a = li[i].getElementsByTagName('a')[0];
+        // let a = li[i].getElementsByTagName('a')[0];
+
+        // You can take textContent
+        let a = li[i].textContent;
+
+        console.log(a);
 
         // if matched
-        if (a.textContent.toUpperCase().indexOf(inputThis) > -1) {
+        if (a.toUpperCase().indexOf(inputThis) > -1) {
             li[i].style.display = '';
         } else {
             li[i].style.display = 'none';
