e2e3443ec1b1e7f82c5a9aa05e45f7c21b177e31
asaduddin <asaduddin.z@samsung.com>
2018-03-01 16:52:53 +0700

    edit app.js Task List

--- Task List/app.js
+++ Task List/app.js
@@ -6,44 +6,86 @@ const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 // Load all event listener
-
+loadEventListeners();
 
 // load all event listener
 function loadEventListeners() {
-
+    // Add Task
+    form.addEventListener('submit', addTask);
+    // Remove Task
+    taskList.addEventListener('click', removeList);
+    // Clear all Task
+    clearBtn.addEventListener('click', removeAll);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
-
-// Add Task
-form.addEventListener('submit', addTask);
 
 function addTask(e) {
 
-    // Create li element
-    const li = document.createElement('li');
-    // Add class
-    li.className = 'collection-item';
-    // Create Text Node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
+    if (taskInput.value !== '') {
+        // Create Text NODE
+        let textNode = document.createTextNode(taskInput.value);
+        // Create li element
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // Create Text Node and append to li
+        li.appendChild(textNode);
 
 
-    // Create new link element
-    const link = document.createElement('a');
-    // Add Class
-    // secondary-content in materialize css make element is right from text
-    link.className = 'delete-item secondary-content';
+        // Create new link element
+        const link = document.createElement('a');
+        // Add Class
+        // secondary-content in materialize css make element is right from text
+        link.className = 'delete-item secondary-content';
 
-    // Add Icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    //Append link to li
-    li.appendChild(link);
+        // Add Icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        //Append link to li
+        li.appendChild(link);
 
-    // Append li to ul
-    taskList.appendChild(li);
+        // Append li to ul
+        taskList.appendChild(li);
 
-    // Clear input
-    taskInput.value = '';
+        // Clear input
+        taskInput.value = '';
 
-    console.log(li)
+        console.log(li)
+    } else {
+        alert('Fill your Tasks');
+    }
 
     e.preventDefault();
+}
+
+function removeList(e) {
+    // Check if contains delete-item or not
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function removeAll() {
+    // empty array slower than removeChild
+    // taskList.innerHTML = '';
+
+    //Fastest using removeChild;
+    while(taskList.hasChildNodes()) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e) {
+    // Get value from input text filter
+    const text = e.target.value.toLowerCase();
+    
+    document.querySelectorAll('.collection-item').forEach(val => {
+        const item = val.firstChild.textContent;
+        
+        if (item.toLowerCase().indexOf(text) !== -1) {
+            val.style.display = 'block';
+        } else {
+            val.style.display = 'none';
+        }
+    })
 }
